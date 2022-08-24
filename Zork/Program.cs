d1c823607aa504c9f3e5b41d733880e5dda55e5e
Alexander Issa <Alex123rocks@gmail.com>
2022-08-24 15:12:24 -0400

    Zork 1.1
    Added Quit and Look functionality (currently doesn't do much)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,7 +6,26 @@ namespace Zork
     {
         static void Main(string[] args)
         {
+            //Welcome message
             Console.WriteLine("Welcome to Zork!");
+            //set up a string variable that we are going to use for user inputs
+            //stopping ___
+            //Trim() gets rid of whitespace (spaces) LeftTrim and RightTrim are also syntax
+            string inputString = Console.ReadLine().Trim(); //.ToUpper() can be placed here
+            inputString = inputString.ToUpper();  //to upper makes everything uppercase so case sensitive stuff is easier to manage
+            //strgcmp is case sensitive
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber may saying 'Welcome to Zork!' lies by the door.");
+            }
+            else
+            {
+                Console.WriteLine($"Unrecognized command: {inputString}"); //the $ makes anything in the {} an interpolating string, which is a string C# tries to convert to an actual string
+            }
         }
     }
 }
