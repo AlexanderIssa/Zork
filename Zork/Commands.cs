92663e1ff7f00e04e5d115794a2e95dd8b59ae97
Alexander Issa <Alex123rocks@gmail.com>
2022-08-29 15:19:52 -0400

    Zork 1.2
    Still in progress, mostly pushing to complete at home

---
+++ Zork/Commands.cs
@@ -0,0 +1,16 @@
+﻿namespace Zork
+{
+    //an enumeration is a collected list of names, can't have the same name in the same enum, PURPOSE IS ONLY TO CREATE A LIST OF RELATED NAMES
+    //this enum is related because they are all commands for our Zork game
+    //enum is a data type
+    public enum Commands //enumeration (enum) is a pascal case
+    {
+        Quit = 0, //don't need = 0, does it automatically
+        Look, //1
+        North,
+        South,
+        East,
+        West,
+        Uknown, //2
+    }
+}
--- Zork/Program.cs
+++ Zork/Program.cs
@@ -13,12 +13,29 @@ namespace Zork
             //Trim() gets rid of whitespace (spaces) LeftTrim and RightTrim are also syntax
             string inputString = Console.ReadLine().Trim(); //.ToUpper() can be placed here
             inputString = inputString.ToUpper();  //to upper makes everything uppercase so case sensitive stuff is easier to manage
-            //strgcmp is case sensitive
+            Commands command = ToCommand(inputString); //created a data type (enumeration) off of the Commands.cs file
+
+            /*
             if (inputString == "QUIT")
+            {
+                command = Commands.Quit;
+            }
+            else if (inputString == "LOOK")
+            {
+                command = Commands.Look;
+            }
+            else
+            {
+                command = Commands.Uknown;
+            }
+            */
+
+            //strgcmp is case sensitive
+            if (command == Commands.Quit)
             {
                 Console.WriteLine("Thank you for playing.");
             }
-            else if (inputString == "LOOK")
+            else if (command == Commands.Look)
             {
                 Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber may saying 'Welcome to Zork!' lies by the door.");
             }
@@ -26,6 +43,36 @@ namespace Zork
             {
                 Console.WriteLine($"Unrecognized command: {inputString}"); //the $ makes anything in the {} an interpolating string, which is a string C# tries to convert to an actual string
             }
+        } //void infront of main is return type so returns nothing
+
+        static Commands ToCommand(string commandString) //this will return a Commands variable
+        {
+
+            if (Enum.TryParse<Commands>(commandString, true, out Commands command))
+            {
+                return command;
+            }
+            else
+            {
+                return Commands.Uknown;
+            }
+
+
+
+            //above code is a simplified version of this below code
+            /*
+            try //try to do this command, if it throws an exception move to catch
+            {
+                return (Commands)Enum.Parse(typeof(Commands), commandString, true);//static method that deals with any type of enum, tell it what type of enum u want to use, can have a third arg that ignores case
+                                                                                   //enum parse returns an object (everything, ALL DATA, is an object)
+            }
+            catch (ArgumentException) //catch that argument exception (inherts from exception) and do what's inside instead
+            {
+                return Commands.Uknown;
+            }
+            */
+
         }
+
     }
 }
