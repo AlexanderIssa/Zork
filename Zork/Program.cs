62f0425ab9fb58ae86cafa12175609f6ed948502
Alexander Issa <Alex123rocks@gmail.com>
2022-08-31 15:17:19 -0400

    Zork 1.3
    + Every current command has a respected output string
    + Quit command stops the program.
    + More comments to help myself understand the code better
    - Removed old commented out code to make blocks cleaner.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,73 +4,61 @@ namespace Zork
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main() //void infront of main is return type so returns nothing (void)
         {
             //Welcome message
             Console.WriteLine("Welcome to Zork!");
-            //set up a string variable that we are going to use for user inputs
-            //stopping ___
-            //Trim() gets rid of whitespace (spaces) LeftTrim and RightTrim are also syntax
-            string inputString = Console.ReadLine().Trim(); //.ToUpper() can be placed here
-            inputString = inputString.ToUpper();  //to upper makes everything uppercase so case sensitive stuff is easier to manage
-            Commands command = ToCommand(inputString); //created a data type (enumeration) off of the Commands.cs file
+            bool isRunning = true;
+            while (isRunning)
+            {
+                //">" to show where player input is being written
+                Console.Write("> ");
+                //set up a string variable that we are going to use for user inputs, stopping ___
+                //Trim() gets rid of whitespace (spaces), LeftTrim and RightTrim are also syntax. To.Upper() makes everything uppercase so case sensitive stuff is easier to manage
+                string inputString = Console.ReadLine().Trim().ToUpper();
+                //create a data type (enumeration) off of the Commands.cs file
+                Commands command = ToCommand(inputString);
 
-            /*
-            if (inputString == "QUIT")
-            {
-                command = Commands.Quit;
-            }
-            else if (inputString == "LOOK")
-            {
-                command = Commands.Look;
-            }
-            else
-            {
-                command = Commands.Uknown;
-            }
-            */
+                string outputString; //assigning a value here would be inefficient as we would be assigning it in all cases in switch below
+                switch (command)
+                {
+                    case Commands.Quit:
+                        isRunning = false; //this will not stop the while loop mid run, the while loop goes through the whole block before updating
+                        outputString = "Thank you for playing!"; //so this will run
+                        break;
 
-            //strgcmp is case sensitive
-            if (command == Commands.Quit)
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (command == Commands.Look)
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber may saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine($"Unrecognized command: {inputString}"); //the $ makes anything in the {} an interpolating string, which is a string C# tries to convert to an actual string
-            }
-        } //void infront of main is return type so returns nothing
+                    case Commands.Look:
+                        outputString = "This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
 
-        static Commands ToCommand(string commandString) //this will return a Commands variable
+                    case Commands.North:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        outputString = $"You moved {command}."; //interpolate the string, very efficient
+                        break;
+                    
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString); //write the output string from respective case
+            } 
+
+        }
+
+        static Commands ToCommand(string commandString) //this method will return a Commands variable
         {
 
-            if (Enum.TryParse<Commands>(commandString, true, out Commands command))
+            if (Enum.TryParse<Commands>(commandString, true, out Commands command)) //if enum TryParse of commands...
             {
                 return command;
             }
-            else
+            else //if that command passed through the commandString is not existent in the Commands.cs file then return Unkown
             {
                 return Commands.Uknown;
             }
-
-
-
-            //above code is a simplified version of this below code
-            /*
-            try //try to do this command, if it throws an exception move to catch
-            {
-                return (Commands)Enum.Parse(typeof(Commands), commandString, true);//static method that deals with any type of enum, tell it what type of enum u want to use, can have a third arg that ignores case
-                                                                                   //enum parse returns an object (everything, ALL DATA, is an object)
-            }
-            catch (ArgumentException) //catch that argument exception (inherts from exception) and do what's inside instead
-            {
-                return Commands.Uknown;
-            }
-            */
 
         }
 
