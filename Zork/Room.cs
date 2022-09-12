c696ecfaf43f75cf8dbf4be644167b72016664a7
Alexander Issa <Alex123rocks@gmail.com>
2022-09-12 15:17:10 -0400

    Zork 3.1 (in progress)
    + Descriptions added but still in progress
    + room.cs file for room class

---
+++ Zork/Room.cs
@@ -0,0 +1,21 @@
+﻿namespace Zork
+{
+    internal class Room
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        //constructor method, must be public, doesn't have a return type, initializes the members of a class
+        public Room(string name)
+        {
+            Name = name;
+        }
+
+        //overrides the ToString() method to return the name of the room
+        public override string ToString()
+        {
+            return Name;
+        }
+    }   
+}
