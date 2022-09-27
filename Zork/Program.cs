0111df75a1045eff0b513b4d763092f144b04225
Alex123rocks <Alex123rocks@gmail.com>
2022-09-26 21:49:24 -0400

    Revert "Merge branch 'main' of https://github.com/Alex123rocks/Zork"
    This reverts commit 64d3c7ef0513a79abc46dc29e8e6f93d4cfb8a55, reversing
    changes made to a69be6bc8d1bb0b56d6c3ab3c2b258108cb31b79.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -16,27 +14,17 @@ namespace Zork
             }
         }
 
-        private static void Main(string[] args) //void infront of main is return type so returns nothing (void), private is implied
+        private static void Main() //void infront of main is return type so returns nothing (void), private is implied
         {
-            string roomsFileName = args.Length > 0 ? args[(int)CommandLineArguments.RoomsFileName] : @"Content\Rooms.json";
-            InitializeRoomDescriptions(roomsFileName);
 
             //Welcome message
             Console.WriteLine("Welcome to Zork!");
-
-            Room previousRoom = null;
+            InitializeRoomDescriptions();
             bool isRunning = true;
             while (isRunning)
             {
                 //">" to show where player input is being written
                 Console.Write($"{CurrentRoom}\n> ");
-                /*
-                if (previousRoom != Player.CurrentRoom)
-                {
-                    Console.WriteLine(Player.CurrentRoom.Description);
-                    previousRoom = Player.CurrentRoom;
-                }
-                */
                 //Trim() gets rid of whitespace (spaces), LeftTrim and RightTrim are also syntax. To.Upper() makes everything uppercase so case sensitive stuff is easier to manage (took out)
                 string inputString = Console.ReadLine().Trim();
                 //create a data type (enumeration) off of the Commands.cs file
@@ -51,7 +39,6 @@ namespace Zork
                         break;
 
                     case Commands.Look:
-                        //outputString = Player.CurrentRoom.Descripton;
                         outputString = CurrentRoom.Description;
                         break;
 
@@ -124,30 +111,30 @@ namespace Zork
             return didMove;
         } //returns true if player moved false if they didn't
 
-        private static void InitializeRoomDescriptions(string roomsFilename)
+        private static void InitializeRoomDescriptions()
         {
-            _rooms[0, 0].Description = "You are on a rock-strewn trail.";
-            _rooms[0, 1].Description = "You are facing the south side of a white house.";
-            _rooms[0, 2].Description = "You are on a rock-strewn trail.";
+            _rooms[0, 0].Description = "You are on a rock-strewn trail."; //Rocky Trail
+            _rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred."; //South of House
+            _rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall."; //Canyon View
 
-            _rooms[1, 0].Description = "You are on a rock-strewn trail.";
-            _rooms[1, 1].Description = "You are on a rock-strewn trail.";
-            _rooms[1, 2].Description = "You are on a rock-strewn trail.";
+            _rooms[1, 0].Description = "This is a forest, with trees in all directions around you."; //Forest
+            _rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door."; //West of House
+            _rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; //Behind House
 
-            _rooms[2, 0].Description = "You are on a rock-strewn trail.";
-            _rooms[2, 1].Description = "You are on a rock-strewn trail.";
-            _rooms[2, 2].Description = "You are on a rock-strewn trail.";
+            _rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight."; //Dense Woods
+            _rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred."; //North of house
+            _rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south."; //Clearing
         }
 
-        private static Room[,] _rooms;
+        //hardcode an array for rooms that is readonly (cant be changed during runtime)
+        //rectangular array, every row has the same amount of columns
+        private static readonly Room[,] _rooms = 
+        {
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
+        };
 
         private static (int Row, int Column) _location = (1, 1); //tuple, two fields, Row and Column
-
-        //1 way to do it
-        //private static int _location.Row = 1;
-        //private static int _location.Column = 1;
-
-        //another way to do it
-        //private static Location _location = new Location() { Row = 1, Column = 1 };
     }
 }
