9c29800ca277f7bf5b5fd856c3f8bffaa6a76bef
Alexander Issa <57016193+Alex123rocks@users.noreply.github.com>
2022-11-16 15:22:02 -0500

    Zork Unity Connection (in progress)
    + Zork.Common connected to Zork.Unity

--- Zork.Unity/Assets/Scripts/GameManager.cs
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zork.Common;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        TextAsset gameJson = Resources.Load<TextAsset>("GameJson");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Game _game;
 }
